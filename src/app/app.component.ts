f7863635ca2a0af6ed2215b7f45544ae9dca1386
Nick Wiesjahn <nick.wiesjahn@cleanslatetg.com>
2024-11-22 12:13:53 -0500

    Push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,10 @@ import { NgIf, NgForOf } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MessageService } from './message.service';
 
+interface Image {
+  image: string;
+  exif: any;
+}
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,20 +14,25 @@ import { MessageService } from './message.service';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
+
 export class AppComponent {
   
   title = 'testapp';
   
-  @Output() image = ''
-  @Output() exif: any;
+  @Output() image: Image = {image: '', exif: {}};
+  @Output() imageGroup: Image[] = [];
   constructor(private messageService: MessageService) {
   
     
   }
   public async getImageFromFlutter(){
-     const result = await this.messageService.recieveImageFromFlutter();
-    this.image= result.image;
-     this.exif = JSON.parse(result.exif);
+     const result: Image[] = await this.messageService.recieveImageFromFlutter();
+     result.forEach((image: Image) => {
+        this.image.image = image.image;
+        this.image.exif = JSON.parse(image.exif);
+        this.imageGroup.push(this.image); 
+     });
   }
   
   objectKeys(obj: any): string[] {
